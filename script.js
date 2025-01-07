b7a76f5c8de1db912a15a894e1fdedbe8be1930a
kumarhasmukh697 <122850621+kumarhasmukh697@users.noreply.github.com>
2025-01-07 12:38:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -119,8 +119,8 @@
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    const brideName = "Lalit";
-    const groomName = "Priyanka";
+    const brideName = "Priyanka";
+    const groomName = "Lalit";
     const weddingDate = new Date("2025-02-07T00:00:00");
     const postWeddingDate = new Date("2025-02-08T00:00:00");
 
